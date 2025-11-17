b99bed9fa813f784f3a47e11f025d6d4d95dcc0e
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-11-17 14:21:58 +0100

    feat: agregar reto 4 en 07_mini_proyectos

---
+++ 07_mini_proyectos/retos/reto4/app.py
@@ -0,0 +1,6 @@
+# Reto 4: Estadísticas del refugio de gatos
+# **Enunciado: **
+# - Entrada: archivo o lista de pesos y edades.
+# - Tareas: conteo, media, mediana simple, top-k más pesados.
+# - Salida: resumen por pantalla y guardado opcional a archivo.
+# **Bonus**: Usa operador morsa para leer el archivo línea por línea.
